df3a3f3edcbc40b0066f30fb8d9bcc7d617f2bfc
Azal-Amer <58004047+Azal-Amer@users.noreply.github.com>
2021-10-22 14:28:07 -0500

    Update BlenderCacheReader.py

--- Blender Implementation/BlenderCacheReader.py
+++ Blender Implementation/BlenderCacheReader.py
@@ -28,7 +28,7 @@ bpy.ops.object.select_all(action="SELECT")
 bpy.ops.object.delete() 
 
 for n in range(0,count):    
-    coordinates = coordinates = (worlds["frames"][0][(n)])
+    coordinates = coordinates = (worlds["frames"][0][n])
     
     bpy.ops.mesh.primitive_cube_add(location=(coordinates[0],coordinates[1],coordinates[2]),size = .5)
     print(n)
@@ -39,43 +39,89 @@ n = 0
   #  print(n)
     
 for object in bpy.context.selected_objects:
-    coordinates = []
+    bpy.ops.object.select_all(action="SELECT")
+    y_cord = []
+    x_cord = []
+    z_cord = []
     for j in range(FrameCount):
-        coordinates.append(worlds['frames'][j][n])
-    
 
-#        world =  bpy.context
-#       world.scene.frame_set(i)
-    # coordinates = (worlds["frames"][(int(i)*skip)][(n)])
-#     object.location.x  = coordinates[0]
-#    object.location.y  = coordinates[1]
-    #   object.location.z  = coordinates[2]
-    
-    
-    #  randColor = random.randint(0,2)
-    # object.keyframe_insert(data_path="location", frame=(key))
-#      if randColor == 0: object.active_material = red 
-#       if randColor == 1: object.active_material = blue
-#        if randColor == 2: object.active_material = green
+        y_cord.append(worlds['frames'][j][n][1])
     frames = range(1, FrameCount-1)
-
     # some action
     a = bpy.data.actions.new(str(n))
-    fc = a.fcurves.new("location", n, "LocY")
+    fc = a.fcurves.new("location", index=1, action_group ="LocY")
 
-    fc.keyframe_points.add(count=len(frames))
+    
     # populate points
 
     fc.keyframe_points.foreach_set("co", 
-            [x for co in zip(frames, coordinates) for x in co])
+            [x for co in zip(frames, y_cord) for x in co])
+
 # update 
     fc.update()
 # assign to context ob
-    ad = bpy.context.object.animation_data_create()
+    ad = object.animation_data_create()
     ad.action = a
-    fc.convert_to_samples(1, 250) 
+    fc.convert_to_samples(1, FrameCount-1) 
     n += 1
-    print(n)
+    
+n=0
+for object in bpy.context.selected_objects:
+    bpy.ops.object.select_all(action="SELECT")
+
+    x_cord = []
+
+    for j in range(FrameCount):
+
+        x_cord.append(worlds['frames'][j][n][0])
+
+    frames = range(1, FrameCount-1)
+    # some action
+    a = bpy.data.actions.new(str(n))
+    fc = a.fcurves.new("location", index=0, action_group ="LocX")
+    fc.keyframe_points.add(count=len(frames))
+
+    
+    # populate points
+
+
+    fc.keyframe_points.foreach_set("co", 
+            [x for co in zip(frames, x_cord) for x in co])
+    
+# update 
+    fc.update()
+# assign to context ob
+    ad = object.animation_data_create()
+    ad.action = a
+    fc.convert_to_samples(1, FrameCount-1) 
+    n += 1
+n=0
+for object in bpy.context.selected_objects:
+    bpy.ops.object.select_all(action="SELECT")
+
+    z_cord = []
+    for j in range(FrameCount):
+
+        z_cord.append(worlds['frames'][j][n][2])
+    
+    frames = range(1, FrameCount-1)
+    # some action
+    a = bpy.data.actions.new(str(n))
+
+    fc = a.fcurves.new("location", index=2, action_group ="LocZ")
+    fc.keyframe_points.add(count=len(frames))
+
+    
+    # populate points  
+    fc.keyframe_points.foreach_set("co", 
+            [x for co in zip(frames, z_cord) for x in co])
+# update 
+    fc.update()
+# assign to context ob
+    ad = object.animation_data_create()
+    ad.action = a
+    fc.convert_to_samples(1, FrameCount-1) 
+    n += 1
 
 
 
